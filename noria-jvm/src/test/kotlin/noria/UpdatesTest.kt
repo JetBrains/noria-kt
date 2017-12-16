d96657fb8f2f13671c785279dc5e055f86edca43
Maxim Shafirov <max@jetbrains.com>
2017-12-16 20:59:44 +0300

    Working on self-descriptive test

--- noria-jvm/src/test/kotlin/noria/UpdatesTest.kt
+++ noria-jvm/src/test/kotlin/noria/UpdatesTest.kt
@@ -8,20 +8,19 @@ import org.jetbrains.noria.createInstance
 import org.jetbrains.noria.reconcile
 import org.jetbrains.noria.with
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+import kotlin.test.fail
 
 class UpdatesTest {
 
     @Test fun `Reconciliation keeps the view and adds update for new subview`() {
         val (component, _) = createInstance(MyMacComponent::class with MyProps())
-
-        val view = (component as UserInstance).view
-        assertTrue(view is MyMacComponent)
+        val view = (component as UserInstance).view as? MyMacComponent ?: fail("View expected to be MyMacComponent")
 
         val (newComponent, updates) = reconcile(component, MyMacComponent::class with MyProps(x = 1))
-        assertTrue((newComponent as UserInstance).view === view)
-
-        val singleUpdate = updates.single()
-        assertTrue(singleUpdate is Update.Add)
+        assertTrue((newComponent as UserInstance).view === view, "View is expected to be kept the same. It's just props shall be changed")
+        assertEquals(1, view.props.x, "Props expected to be updated")
+        assertTrue(updates.single() is Update.Add, "There should be single update of adding subview")
     }
 }
